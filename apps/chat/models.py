ac1397d168fd1aca147925be1860e3d27c5ced14
prashg008 <prashanthg008@gmail.com>
2020-04-24 12:56:28 +0530

    added chat model

---
+++ apps/chat/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class Rooms(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    members = models.ManyToManyField(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Messages(models.Model):
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    user = models.ForeignKey(User)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
